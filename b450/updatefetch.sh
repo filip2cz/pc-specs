2363588f782d7d016957d5c446d9120e42515aa8
filip2cz <filip@fkomarek.eu>
2026-04-10 11:35:04 +0200

    DEL: uptime

--- b450/updatefetch.sh
+++ b450/updatefetch.sh
@@ -1,6 +1,6 @@
 echo "[filip@b450:~]$ fastfetch" > fetch.txt
 echo >> fetch.txt
-fastfetch --logo none -s Title:Separator:OS:Host:Kernel:Uptime:Packages:Shell:Display:DE:WM:WMTheme:Theme:Icons:Font:Cursor:Terminal:TerminalFont:CPU:GPU:Memory:Swap:Disk:LocalIP:Battery:PowerAdapter:Locale:Break >> fetch.txt
+fastfetch --logo none -s Title:Separator:OS:Host:Kernel:Packages:Shell:Display:DE:WM:WMTheme:Theme:Icons:Font:Cursor:Terminal:TerminalFont:CPU:GPU:Memory:Swap:Disk:LocalIP:Battery:PowerAdapter:Locale:Break >> fetch.txt
 echo >> fetch.txt
 
 echo "[filip@b450:~]$ cpufetch" >> fetch.txt
